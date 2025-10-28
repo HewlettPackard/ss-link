248ec335e31d7c73fac3c996123b0d96c9ddcb5d
Eric Thomas <eric.thomas@hpe.com>
2025-10-28 16:22:13 -0600

    [1.29.38] SSHOTPFCMN-282: update cable DB
    Signed-off-by: Eric Thomas <eric.thomas@hpe.com>

--- knl/sysfs/sl_sysfs_ldev.c
+++ knl/sysfs/sl_sysfs_ldev.c
@@ -308,7 +308,7 @@ int sl_sysfs_ldev_create(u8 ldev_num, struct kobject *parent)
 
 	if (!sl_ctl_ldev_kref_get_unless_zero(ctl_ldev)) {
 		sl_log_err(ctl_ldev, LOG_BLOCK, LOG_NAME,
-			"kref_get_unless_zero failed (ldev = 0x%p)", ctl_ldev);
+			   "kref_get_unless_zero failed (ldev = 0x%p)", ctl_ldev);
 		return -EBADRQC;
 	}
 
