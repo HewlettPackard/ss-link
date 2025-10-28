130b5dd95705da498761ba6752cd4d2f8516beef
Eric Thomas <eric.thomas@hpe.com>
2025-10-28 16:22:13 -0600

    [1.30.121] SSHOTPFCMN-282: update cable DB

--- drivers/net/ethernet/hpe/sl/sl.h
+++ drivers/net/ethernet/hpe/sl/sl.h
@@ -6,7 +6,7 @@
 
 #define SL_VERSION_MAJOR 1
 #define SL_VERSION_MINOR 30
-#define SL_VERSION_INC   120
+#define SL_VERSION_INC   121
 
 void  sl_version_get(int *major, int *minor, int *inc);
 char *sl_git_hash_str_get(void);
