8ef8e4128b555196ad2166c30faa9e7789c008af
Shashidhar Ramreddy <shashidhar.ramreddy@hpe.com>
2026-05-12 15:02:29 -0500

    [1.33.48] SSHOTPFCMN-504: link_up fail history doesn't populate correctly
    Signed-off-by: Shashidhar Ramreddy <shashidhar.ramreddy@hpe.com>

--- drivers/net/ethernet/hpe/sl/sl.h
+++ drivers/net/ethernet/hpe/sl/sl.h
@@ -6,7 +6,7 @@
 
 #define SL_VERSION_MAJOR 1
 #define SL_VERSION_MINOR 33
-#define SL_VERSION_INC   47
+#define SL_VERSION_INC   48
 
 void  sl_version_get(int *major, int *minor, int *inc);
 char *sl_git_hash_str_get(void);
